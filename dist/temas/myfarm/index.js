0a30a4c6da9cda38e366a5dbb165c108a32f2732
Hugo Oliveira <hugo.oliveira@siagri.local>
2019-02-11 17:17:20 -0200

    Add files templates

--- dist/temas/myfarm/index.js
+++ dist/temas/myfarm/index.js
@@ -272,39 +272,6 @@ if (typeof window !== "undefined" && window.Vue) {
 
 /***/ }),
 
-/***/ "TVIv":
-/*!*******************************************************!*\
-  !*** ./src/temas/myfarm/components/select/Select.vue ***!
-  \*******************************************************/
-/*! exports provided: default */
-/*! ModuleConcatenation bailout: Module is referenced from these modules with unsupported syntax: ./src/temas/myfarm/components sync \.vue$ (referenced with context element) */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "KHd+");
-var render, staticRenderFns
-var script = {}
-
-
-/* normalize component */
-
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_0__["default"])(
-  script,
-  render,
-  staticRenderFns,
-  false,
-  null,
-  null,
-  null
-  
-)
-
-component.options.__file = "Select.vue"
-/* harmony default export */ __webpack_exports__["default"] = (component.exports);
-
-/***/ }),
-
 /***/ "X7fK":
 /*!**********************************************************************************************!*\
   !*** ./src/temas/myfarm/components/uploader/uploader.scss?vue&type=style&index=0&lang=scss& ***!
@@ -357,7 +324,6 @@ __webpack_require__.r(__webpack_exports__);
 var map = {
 	"./button/Button.vue": "hGyZ",
 	"./inputs/Input.vue": "ylCN",
-	"./select/Select.vue": "TVIv",
 	"./uploader/Uploader.vue": "eBMG"
 };
 
@@ -475,7 +441,7 @@ component.options.__file = "Uploader.vue"
   \*******************************************************************/
 /*! exports provided: default */
 /*! ModuleConcatenation bailout: Cannot concat with ./src/temas/myfarm/components/button/Button.vue?vue&type=custom&index=0&blockType=docs (<- Module is not an ECMAScript module) */
-/*! ModuleConcatenation bailout: Cannot concat with ./node_modules/vue-loader/lib/runtime/componentNormalizer.js because of ./src/temas/myfarm/components/select/Select.vue */
+/*! ModuleConcatenation bailout: Cannot concat with ./node_modules/vue-loader/lib/runtime/componentNormalizer.js because of ./src/temas/myfarm/components/inputs/Input.vue */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
