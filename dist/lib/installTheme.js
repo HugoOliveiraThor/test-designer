f141cd3f20b9c5424118b771a3fc18ab80b7f938
Hugo Oliveira <hugo.oliveira@siagri.local>
2019-02-13 10:15:31 -0200

    update files1

--- dist/lib/installTheme.js
+++ dist/lib/installTheme.js
@@ -2,13 +2,15 @@
 const chalk = require("chalk")
 const clear = require("clear")
 const figlet = require("figlet")
-
+const path = require("path")
 const repo = require("./repo")
 const verifyPath = require("./files")
 
 clear()
 console.log(chalk.yellow(figlet.textSync("Conexa SDK", { horizontalLayout: "full" })))
 
-const run = async () => verifyPath.directoryExists("./dist/temas/") && (await repo.setTheme())
+// Não adianta rodar o projeto a partir do sdk não ira funcionar , somente a partir da instalação do node_modules
+const run = async () =>
+  verifyPath.directoryExists(path.join(__dirname, "../temas")) && (await repo.setTheme())
 
 run()
